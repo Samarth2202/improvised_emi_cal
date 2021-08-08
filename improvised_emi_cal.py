5529e4a143e06ac7160059e7093db624f4f794d0
Samarth2202 <88436873+Samarth2202@users.noreply.github.com>
2021-08-08 14:41:16 +0530

    Add files via upload

--- improvised_emi_cal.py
+++ improvised_emi_cal.py
@@ -9,7 +9,7 @@ def calculate_outstanding_balance(p, n, r, m):
   return round((p*((r/100)**n) - ((1+(r/100))**m))/((1+(r/100))**n - 1),2)
 
 st.title('EMI Calaculator')
-principal = st.sidebar.slider('Principal Loan Amount',1000,{100000:,2f})
+principal = st.sidebar.slider('Principal Loan Amount',1000,100000)
 tenure = st.sidebar.slider('Loan Period(in years)',1,30)
 n = tenure*12
 roi = st.sidebar.slider('Rate of Interest(in % per annum)',1,15)
