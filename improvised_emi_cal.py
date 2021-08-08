f9392f31e9445c0b0fcabdaf46c7e356ffe6f575
Samarth2202 <88436873+Samarth2202@users.noreply.github.com>
2021-08-08 14:58:28 +0530

    Add files via upload

--- improvised_emi_cal.py
+++ improvised_emi_cal.py
@@ -1,3 +1,4 @@
+# Write the code for creating the Improvised EMI calculator app
 import streamlit as st
 
 @st.cache()
@@ -6,15 +7,15 @@ def calculate_emi(p, n, r):
 
 @st.cache()
 def calculate_outstanding_balance(p, n, r, m):
-  return round((p*((r/100)**n) - ((1+(r/100))**m))/((1+(r/100))**n - 1),2)
+  return round((p*((1 +( r/100))**n) - ((1+(r/100))**m))/((1+(r/100))**n - 1),2)
 
-st.title('EMI Calaculator')
+st.title('Improvised EMI Calaculator')
 principal = st.sidebar.slider('Principal Loan Amount',1000,100000)
 tenure = st.sidebar.slider('Loan Period(in years)',1,30)
 n = tenure*12
 roi = st.sidebar.slider('Rate of Interest(in % per annum)',1,15)
 r = roi/12
-m = st.sidebar.slider('Period after which the Outstanding Loan Balance is calculated (in months)',1,n,(0.25,0.5,0.75))
+m = st.sidebar.slider('Period after which the Outstanding Loan Balance is calculated (in months)',1,n,(0.25,0.5))
 if st.sidebar.button('Calculate EMI'):
   'Your EMI is : ',calculate_emi(principal, n, r)
 if st.sidebar.button('Calculate Outstaning Loan Balance'):
